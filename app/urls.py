37266e81fc373db124a7f24692fe607fd5715744
suyog ojha <ojhasuyog2020@gmail.com>
2022-11-23 14:06:34 +0545

    !added

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from app import views
 from . import views
-
 
 
 urlpatterns = [
     path("", views.home),
+    path("about/", views.About, name="about"),
+    path("contact/", views.Contact, name="contact"),
     path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
     path("category-title/<val>", views.CategoryTitle.as_view(), name="category-title"),
     path("product-details/<int:pk>", views.ProductDetails.as_view(), name="product-details"),
+    
+    
+    #login authentication
 ]
--- app/views.py
+++ app/views.py
@@ -2,13 +2,22 @@ from django.shortcuts import render
 from urllib import request
 from django.http import HttpResponse
 from django.views import View
-from .models import *
+from app.models import *
+
 from django.db.models import Count
 
 
 # Create your views here.
 def home(request):
     return render(request, "app/home.html")
+
+
+def About(request):
+    return render(request, "app/about.html")
+
+
+def Contact(request):
+    return render(request, "app/contact.html")
 
 class CategoryView(View):
     def get(self, request, val):
@@ -28,4 +37,5 @@ class ProductDetails(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
         return render(request, "app/productdetail.html", locals())
+        
         
